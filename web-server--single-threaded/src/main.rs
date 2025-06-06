aedfb9746709a1cabef75ade689769dd2fff3867
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-06-06 16:22:18 -0700

    render webpage

--- web-server--single-threaded/src/main.rs
+++ web-server--single-threaded/src/main.rs
@@ -1,10 +1,10 @@
 //https://doc.rust-lang.org/book/ch21-00-final-project-a-web-server.html
 
 use std::{
+    fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
 };
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -23,5 +23,12 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {http_request:#?}");
-}
+        let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+    }
